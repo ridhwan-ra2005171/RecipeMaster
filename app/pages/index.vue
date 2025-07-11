20ccf412423061ee062cb68531655ac5ce844444
ridhwan-ra2005171 <ra2005171@qu.edu.qa>
2025-07-11 22:41:29 +0300

    dynamic routing

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -53,11 +53,11 @@ const{data, error} = await useFetch<RecipeResponse>('https://dummyjson.com/recip
                 <span>{{ recipe.rating }}</span>
               </div>
             </div>
-            <button
+            <NuxtLink :to="`/recipes/${recipe.id}`"
               class="px-4 py-2 text-white self-start bg-dodgeroll-gold rounded-md text-base lg:text-lg cursor-pointer"
             >
               View
-            </button>
+            </NuxtLink>
           </div>
         </div>
       </div>
---
+++ app/pages/recipes/[id].vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import type { RecipeResponse } from '~~/types/types';
+
+
+const {recipeId} = useRoute().params;
+
+const {data, error} = await useFetch<RecipeResponse>(`https://dummyjson.com/recipes/${recipeId}`);
+
+</script>
+
+<template>
+  <div>
+    <h1 class="text-4xl">{{ recipeId }}</h1>
+  </div>
+</template>
