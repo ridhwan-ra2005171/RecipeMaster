02478ad58463a2a3505e8367eaed1d66fc67ff1a
ridhwan-ra2005171 <ra2005171@qu.edu.qa>
2025-07-11 16:39:36 +0300

    modules setup
    check tailwind.config.js (container)

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="text-lg font-bold text-blue-500">Hello Worlds</h1>
   </div>
 </template>
+<!-- tailwindCSS.experimental.configFile remove this -->
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,41 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
-})
+
+  future: {
+    compatibilityVersion: 4,
+  },
+
+  experimental: {
+    sharedPrerenderData: false,
+    compileTemplate: true,
+    resetAsyncDataToUndefined: true,
+    templateUtils: true,
+    relativeWatchPaths: true,
+    defaults: {
+      useAsyncData: {
+        deep: true,
+      },
+    },
+  },
+
+  unhead: {
+    renderSSRHeadOptions: {
+      omitLineBreaks: false,
+    },
+  },
+
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/google-fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+  ],
+
+  googleFonts: {
+    families: {
+      'Montserrat': true,
+    },
+  },
+});
